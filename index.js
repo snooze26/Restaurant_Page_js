fcd7666090788c51e0e70c66fafa822af7529895
snooze26 <langdon.douglas26@gmail.com>
2024-11-26 19:03:44 -0600

    add entry and dist js files, remove html text

---
+++ index.js
@@ -0,0 +1,3 @@
+import loadHome from './loadHome.js';
+
+loadHome();
---
+++ loadHome.js
@@ -0,0 +1,14 @@
+export default function loadHome(){
+    const content = document.querySelector("#content");
+    
+    const header = document.createElement("h1");
+    header.textContent = "Come eat at the Krusty Krab!";
+
+
+    const pizza = document.createElement("div");
+    pizza.textContent = "Now serving KK pizza";
+    pizza.classList.add("pizza_announcement")
+
+    content.append(header);
+    content.append(pizza);
+}
